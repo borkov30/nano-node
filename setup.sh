a2dd0252a00a7151d8bc146b10838b6edf8c3d3f
lephleg <lephleg@gmail.com>
2018-11-11 04:40:36 +0200

    Implement automatic seed import mode.

--- setup.sh
+++ setup.sh
@@ -14,14 +14,17 @@ reset=`tput sgr0`
 quiet='false'
 displaySeed='false'
 fastSync='false'
+importSeed=''
+printImportInstructions='false';
 domain=''
 email=''
 tag=''
-while getopts 'sqfd:e:t:' flag; do
+while getopts 'sqfd:e:t:i:' flag; do
   case "${flag}" in
     s) displaySeed='true' ;;
     d) domain="${OPTARG}" ;;
     e) email="${OPTARG}" ;;
+    i) importSeed="${OPTARG}" ;;
     q) quiet='true' ;;
     f) fastSync='true' ;;
     t) tag="${OPTARG}" ;;
@@ -152,18 +155,52 @@ done
 # WALLET SETUP
 existedWallet="$(docker exec -it nano-node /usr/bin/rai_node --wallet_list | grep 'Wallet ID' | awk '{ print $NF}')"
 
-if [[ ! $existedWallet ]]; then
-    [[ $quiet = 'false' ]] && printf "${yellow}No wallet found. Generating a new one... ${reset}"
+if [[ $importSeed ]]; then
 
-    walletId=$(docker exec -it nano-node /usr/bin/rai_node --wallet_create | tr -d '\r')
-    address=$(docker exec -it nano-node /usr/bin/rai_node --account_create --wallet=$walletId | awk '{ print $NF}')
-    
-    [[ $quiet = 'false' ]] && printf "${green}done.${reset}\n"
+    if [[ ${#importSeed} = 64 ]]; then
+
+        [[ $quiet = 'false' ]] && printf "${yellow}Import seed enabled. Importing wallet... ${reset}"
+
+        if [[ ! $existedWallet ]]; then
+            walletId=$(docker exec -it nano-node /usr/bin/rai_node --wallet_create | tr -d '\r')
+        else
+            walletId=$(echo $existedWallet | tr -d '\r')
+        fi
+
+        docker exec -it nano-node /usr/bin/rai_node --wallet_change_seed --wallet=$walletId --key=$importSeed
+        address="$(docker exec -it nano-node /usr/bin/rai_node --wallet_list | grep 'xrb_' | awk '{ print $NF}' | tr -d '\r')"
+
+        [[ $quiet = 'false' ]] && printf "${green}done.${reset}\n"
+
+    else 
+
+        [[ $quiet = 'false' ]] && printf "${yellow}Import seed enabled. However, no wallet seed was passed or it was invalid. Installer will create a temporary wallet and guide you how to manually import your seed... ${reset}"
+
+        walletId=$(docker exec -it nano-node /usr/bin/rai_node --wallet_create | tr -d '\r')
+        address=$(docker exec -it nano-node /usr/bin/rai_node --account_create --wallet=$walletId | awk '{ print $NF}')
+
+        printImportInstructions='true';
+
+        [[ $quiet = 'false' ]] && printf "${green}done.${reset}\n"
+
+    fi
+
 else
-    [[ $quiet = 'false' ]] && echo "${yellow}Existing wallet found.${reset}"
 
-    address="$(docker exec -it nano-node /usr/bin/rai_node --wallet_list | grep 'xrb_' | awk '{ print $NF}' | tr -d '\r')"
-    walletId=$(echo $existedWallet | tr -d '\r')
+    if [[ ! $existedWallet ]]; then
+        [[ $quiet = 'false' ]] && printf "${yellow}No wallet found. Generating a new one... ${reset}"
+
+        walletId=$(docker exec -it nano-node /usr/bin/rai_node --wallet_create | tr -d '\r')
+        address=$(docker exec -it nano-node /usr/bin/rai_node --account_create --wallet=$walletId | awk '{ print $NF}')
+        
+        [[ $quiet = 'false' ]] && printf "${green}done.${reset}\n"
+    else
+        [[ $quiet = 'false' ]] && echo "${yellow}Existing wallet found.${reset}"
+
+        address="$(docker exec -it nano-node /usr/bin/rai_node --wallet_list | grep 'xrb_' | awk '{ print $NF}' | tr -d '\r')"
+        walletId=$(echo $existedWallet | tr -d '\r')
+
+    fi
 
 fi
 
@@ -220,7 +257,7 @@ sed -i -e 's/\r//g' ./nano-node-monitor/config.php
 [[ $quiet = 'false' ]] && printf "${green}done.${reset}\n"
 
 if [[ $quiet = 'false' ]]; then
-    echo "${yellow} ---------------------------------------------------------------------"
+    echo "${yellow} --------------------------------------------------------------------- ${reset}"
     echo "${green} ${bold}Congratulations! NANO Node Docker stack has been setup successfully!${reset}"
     echo "${yellow} --------------------------------------------------------------------- ${reset}"
     if [[ $domain ]]; then
@@ -229,4 +266,15 @@ if [[ $quiet = 'false' ]]; then
         echo "${yellow}Open a browser and navigate to ${green}http://$ipAddress${yellow} to check your monitor."
     fi
     echo "${yellow}You can further configure and personalize your monitor by editing the config file located in ${green}nano-node-monitor/config.php${yellow}.${reset}"
+
+    if [[ $printImportInstructions = 'true'; ]] then
+        echo "${yellow} --------------------------------------------------------------------- ${reset}"
+        echo "${yellow} ${bold}In order to import your existing wallet seed use the following command:${reset}"
+        echo "${yellow} --------------------------------------------------------------------- ${reset}"
+        echo "${green} docker exec -it nano-node /usr/bin/rai_node --wallet_change_seed --wallet=${yellow}$walletId${green}--key=${yellow}<YOUR_SEED> ${reset}"
+        echo "${yellow} --------------------------------------------------------------------- ${reset}"
+        echo "${yellow}Afterwards, you will have to update your monitor with the correct NANO address (\$nanoNodeAccount) by editing the config file located in ${green}nano-node-monitor/config.php${yellow}.${reset}"
+    fi
+]]
+
 fi
